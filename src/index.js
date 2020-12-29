a72f6cada9e9304c579d47446d09753fe9363e96
Shierhier <59607443+Shierhier@users.noreply.github.com>
2020-12-29 13:01:31 +0100

    Added relevant icon

--- src/index.js
+++ src/index.js
@@ -23,14 +23,19 @@ function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     cityElement.innerHTML = response.data.name;
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     descriptionElement.innerHTML = response.data.weather[0].description;
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ec260614c11f0896fd34ffe37c5a9a38";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
